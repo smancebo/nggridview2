7a97c67762bf661ae2acf3818d573ead8a091790
Samuel Mancebo <smancebo@cosefi.com>
2015-09-28 17:34:38 -0400

    compleated grid sorter and paginator

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,38 @@ app.controller('textController', ['$scope', function ($scope) {
     $scope.form.directory = [
         { name: 'Samuel', phone: '809-566-5985' },
         { name: 'Jeancarlos', phone: '809-566-5985' },
-        { name: 'mancebo', phone: '809-566-5985' },
-        { name: '', phone: '809-566-5985' },
-        { name: 'Samuel', phone: '809-566-5985' }
+        { name: 'mancebo', phone: '1849-566-5985' },
+        { name: '', phone: '1337-566-5985' },
+        { name: '', phone: '1809-566-5985' },
+        { name: '', phone: '1475-588-5988' },
+        { name: 'Samuel', phone: '009-566-5985' },
+         { name: 'Samuel', phone: '809-566-5985' },
+        { name: 'Jeancarlos', phone: '809-566-5985' },
+        { name: 'mancebo', phone: '1849-566-5985' },
+        { name: '', phone: '1337-566-5985' },
+        { name: '', phone: '1809-566-5985' },
+        { name: '', phone: '1475-588-5988' },
+        { name: '', phone: '1475-588-5988' },
+        { name: 'Samuel', phone: '009-566-5985' }
     ]
 
     $scope.pruebame = function (name) {
-        debugger
-        alert('esta es mi prueba hehehe ' + name);
+        console.log($scope);
     }
+
+    $scope.addValue = function () {
+        //$scope.form.directory.push({name: 'prueba2', phone: '12345679'})
+        console.log($scope);
+    }
+
+    $scope.addPage = function () {
+        $scope.gvCurrentPage++;
+    }
+
+    $scope.removePage = function () {
+        $scope.gvCurrentPage--;
+    }
+
 
     $scope.procesar = function (name) {
         return 'procesado el nombre de ' + name;
--- assets/js/ng-grid-view.js
+++ assets/js/ng-grid-view.js
@@ -3,6 +3,83 @@
 var app = angular.module('ngGridView', []);
 
 
+app.filter('offset', function () {
+    return function (input, start) {
+        start = parseInt(start, 10);
+        return input.slice(start);
+    };
+});
+
+app.directive('gridPaginator', [function () {
+    return {
+        restrict: 'E',
+        scope:{
+            pageSize:'@',
+            currentPage: '=',
+            source: '=',
+            onPageSelected:'&?'
+        },
+        controller: ['$scope', function ($scope) {
+
+            
+            $scope.totalPage = ($scope.source.length / parseInt($scope.pageSize,10));
+            if ($scope.totalPage > Math.round($scope.totalPage)) {
+                $scope.totalPage = Math.round($scope.totalPage) + 1;
+            } else
+            {
+                $scope.totalPage = Math.round($scope.totalPage);
+            }
+
+            $scope.pageCount = [];
+            for (var i = 0; i <= $scope.totalPage - 1; i++) {
+                $scope.pageCount.push(i);
+            }
+
+            $scope.firstPage = function () {
+                $scope.selectCurrentPage(0);
+            }
+
+            $scope.lastPage = function () {
+                $scope.selectCurrentPage($scope.pageCount.length - 1);
+            }
+
+            $scope.nextPage = function () {
+                var next = $scope.currentPage.pageNumber + 1;
+                if (next >= $scope.pageCount.length) {
+                    next = $scope.pageCount.length - 1;
+                }
+                $scope.selectCurrentPage(next);
+            };
+
+            $scope.prevPage = function () {
+                var back = $scope.currentPage.pageNumber - 1;
+                if (back <= 0) {
+                    back = 0;
+                }
+                $scope.selectCurrentPage(back);
+            };
+
+            $scope.selectCurrentPage = function (page) {
+                
+                $scope.currentPage.pageNumber = page;
+                if ($scope.onPageSelected) {
+                    $scope.onPageSelected(page);
+                }
+            }
+        }],
+        template: "<nav>" +
+                        "<ul class='pagination'>" +
+                            "<li><a href='#' ng-click='firstPage()'><span>&laquo;</span></a></li>" +
+                            "<li><a href='#' ng-click='prevPage()'><i class='glyphicon glyphicon-chevron-left'></i></a></li>" +
+                            "<li ng-class='{\"active\" : page == currentPage.pageNumber}' ng-repeat='page in pageCount'><a href='#' ng-click='selectCurrentPage(page)'>{{page+1}}</a></li>" +
+                            "<li><a href='#' ng-click='nextPage()'><i class='glyphicon glyphicon-chevron-right'></i></a></li>" +
+                            "<li><a href='#' ng-click='lastPage()'><span>&raquo;</span></a></li>" +
+                        "</ul>" + 
+                  "</nav>"
+                    
+    }
+}]);
+
 app.directive('compile', ['$compile', '$parse', '$interpolate', function ($compile, $parse, $interpolate) {
 
     return {
@@ -25,7 +102,7 @@ app.directive('gridColumn', ['$compile', '$interpolate', function ($compile, $in
         template: "<div ng-switch on='column.type'>" +
                         "<div ng-switch-when='template' compile='{{column.template}}'></div>" +
                         "<div ng-switch-when='bound'>" +
-                            "{{ row[column.dataField] || column.text || column.nullText }}" +
+                            "{{  column.text || row[column.dataField] || column.nullText }}" +
                         "</div>" +
                         "<div ng-switch-when='counter'>" +
                             "{{ $parent.$parent.$index + 1 }}" +
@@ -34,6 +111,7 @@ app.directive('gridColumn', ['$compile', '$interpolate', function ($compile, $in
         replace: true,
         
         link: function ($scope, element, attr) {
+            $scope.column.sorter = $scope.column.dataField;
             $scope.column = angular.copy($scope.column);
             $scope.$parent.row.$even = $scope.$parent.$even;
             $scope.$parent.row.$first = $scope.$parent.$first;
@@ -43,20 +121,53 @@ app.directive('gridColumn', ['$compile', '$interpolate', function ($compile, $in
             $scope.$parent.row.$middle = $scope.$parent.$middle;
             $scope.$parent.row.$odd = $scope.$parent.$odd;
 
+            
+
             if ($scope.column.type == 'bound') {
+
                 $scope.column.text = $interpolate($scope.column.text)($scope.$parent.row);
                 $scope.column.nullText = $interpolate($scope.column.nullText)($scope.$parent.row);
+                $scope.$parent.row[$scope.column.dataField] = $scope.$parent.row[$scope.column.dataField] == '' ? $scope.column.nullText : $scope.$parent.row[$scope.column.dataField];
             }
         }
     }
 
 }])
 
-app.directive('gridView', ['$compile', '$parse', function ($compile, $parse) {
+app.directive('gridView', ['$compile', '$parse', '$interpolate', function ($compile, $parse, $interpolate) {
 
     return {
 
         restrict: 'E',
+        controller: ['$scope', function ($scope) {
+            
+            $scope.sortTable = function (sortExpression) {
+
+                if (sortExpression == $scope.sorter) {
+                    $scope.sorter = '-' + sortExpression;
+                }
+                else
+                {
+                    $scope.sorter = sortExpression;
+                }
+            }
+
+            $scope.isDesc = function (sortExpression) {
+                
+                if ($scope.sorter == undefined)
+                {
+                    return false;
+                }
+                if ($scope.sorter.indexOf('-') == -1) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+        }],
         compile: function (element, attrs) {
 
             var gv = "";
@@ -71,7 +182,8 @@ app.directive('gridView', ['$compile', '$parse', function ($compile, $parse) {
                 pageSize: attrs.pageSize || 10,
                 emptyText: attrs.emptyText || ''
             }
-            
+
+          
 
             /*
             allow-sorting="" allow-pagging="" allow-search="" page-size="10" gvdata-source="form.directory" empty-text=""
@@ -83,31 +195,63 @@ app.directive('gridView', ['$compile', '$parse', function ($compile, $parse) {
                 "<table class='table table-striped'>" +
                     "<thead>" +
                         "<tr>" +
-                            "<th class='gv-header' ng-repeat='column in columns'>" + 
-                                "{{column.headerText}}<i ng-if='column.sorteable' style='float:right; font-size:10px' class='glyphicon glyphicon-triangle-bottom'></i>" +
+                            "<th class='gv-header' ng-repeat='column in columns'>" +
+                                "<div ng-if='" + config.allowSorting + "' ng-click='sortTable(column.sorter)'>" +
+                                    "{{column.headerText}}" +
+                                    "<div style='float:right;'>" +
+                                        "<i ng-hide='' ng-if='column.sorteable' style='display:block; font-size:10px' class='glyphicon glyphicon-triangle-top'></i>" +
+                                        "<i ng-hide='' ng-if='column.sorteable' style='display:block; font-size:10px' class='glyphicon glyphicon-triangle-bottom'></i>" +
+                                    "</div>" +
+                                "</div>" +
+                                "<div ng-if='!" + config.allowSorting + "'>" +
+                                    "{{column.headerText}}" +
+                                "</div>" +
                             "</th>" +
                         "</tr>" +
                     "</thead>" +
                     "<tbody>" +
-                        "<tr ng-repeat='row in " + config.gvdataSource + "'>" +
+                        "<tr ng-repeat='row in " + config.gvdataSource + " | orderBy:sorter | offset: gvCurrentPage.pageNumber*gvPageSize | limitTo: gvPageSize'>" +
                             "<td ng-repeat='column in columns'>" +
-                                "<grid-column column='column' row='row'></grid-column>"+
+                                "<grid-column column='column' row='row'></grid-column>" +
                             "</td>" +
                         "</tr>" +
                     "</tbody>" +
+                    "<tfoot>" +
+                        "<tr>" +
+                            "<td colspan='{{columns.length}}'>" +
+                                "<grid-paginator current-page='gvCurrentPage' source='" + config.gvdataSource + "' page-size='" + config.pageSize + "'  ng-if='" + config.allowPagging + "'></grid-paginator>" +
+                            "</td>" +
+                        "</tr>" +
+                    "</tfoot>" +
                 "</table>" +
+                
             "</div>";
 
             element.find('columns').children().each(function () {
+                
                 var column = {};
-                column.headerText = $(this).attr('header-text');
-                column.dataField = $(this).attr('data-field');
+                column.headerText = $(this).attr('header-text') || '';
+                column.dataField = $(this).attr('data-field') || '';
                 column.text = $(this).attr('text') || '';
                 column.nullText = $(this).attr('null-text') || '';
-                column.sorteable = $(this).attr('sorteable');
-                column.counter = $(this).attr('counter');
-
+                column.sorteable = $(this).attr('sorteable') == undefined ? 'true' : $(this).attr('sorteable');
+                column.counter = $(this).attr('counter') || false;
                 var columnTag = $(this)[0].tagName.toLowerCase();
+
+                
+                if ((column.dataField == '' || column.dataField == undefined) && column.counter == false && columnTag != 'templatecolumn') {
+                    throw "Columns require the attribute 'data-field'"
+                }
+
+                if (column.sorteable === 'false') {
+                    column.sorteable = false;
+                } else if (column.sorteable === 'true') {
+                    column.sorteable = true;
+                }
+                else {
+                    throw 'Sorteable value not valid, should be true or false and has ' + column.sorteable;
+                }
+
 
                 switch (columnTag) {
                     case 'column':
@@ -117,11 +261,13 @@ app.directive('gridView', ['$compile', '$parse', function ($compile, $parse) {
                     case 'templatecolumn':
                         column.type = 'template'
                         column.template = $(this).html();
+                        column.sorteable = false;
                         break;
                 }
 
                 if (column.counter === 'true') {
-                    column.type = 'counter'
+                    column.type = 'counter';
+                    column.sorteable = false;
                 }
 
                 data.columns.push(column);
@@ -129,7 +275,19 @@ app.directive('gridView', ['$compile', '$parse', function ($compile, $parse) {
 
 
             return function ($scope, element, attrs) {
+                if (config.allowPagging === 'false') {
+                    $scope.gvPageSize = null;
+                }
+                else
+                {
+                    $scope.gvPageSize = config.pageSize;
+                }
+
                 $scope.columns = data.columns;
+                
+                $scope.gvCurrentPage = {};
+                $scope.gvCurrentPage.pageNumber = 0;
+
                 element.replaceWith($compile(template)($scope));
             }
 
